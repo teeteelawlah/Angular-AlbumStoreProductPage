2dd97e9950351a755016d3eafeebce5d9f57024a
Akinwale Mutiat <mutiatakinwale@gmail.com>
2020-09-29 17:20:24 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,9 +8,9 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number): {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
-    response.json());
+    <Album>response.json());
   }
 
 }
